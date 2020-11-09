81b1197bf43f8c283996a186e0ebabe0ae6fe573
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-11-08 18:02:38 -0600

    Adds alias database-related *Adds alias for "sudo pacman -D --asexplicit" *Adds alias for "sudo pacman -D --asdeps"

--- init.zsh
+++ init.zsh
@@ -106,6 +106,16 @@
   # remove orphan packages
   alias pacor="${zpacman_frontend_priv} -Rns \$(pacman -Qtdq)"
 
+  #
+  #Database
+  #
+
+  #mark packages as explicitly installed
+  alias pacpexp="${zpacman_frontend_priv} -D --asexplicit"
+
+  #mark packages as non-explicitly installed or dependency
+  alias pacpdep="${zpacman_frontend_priv} -D --asedps"
+
 
   #
   # Ownership
