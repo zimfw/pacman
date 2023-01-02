f4b45a8c3b47c33a5e5b1e370c4c68e16a8796d1
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2023-01-02 11:15:57 -0500

    Remove extra quotes
    Still only leave redundant quotes in
    
        zstyle -s 'context' style 'name'
    
    as that's kind of a convention already.

--- init.zsh
+++ init.zsh
@@ -14,25 +14,25 @@ if (( ! ${+commands[pacman]} )); then
 fi
 
 if ! zstyle -s ':zim:pacman' priv_cmd 'zpriv_cmd'; then
-  zpriv_cmd='sudo'
+  zpriv_cmd=sudo
 elif [[ ${zpriv_cmd} != (sudo|doas) ]]; then
   print -u2 "Privilege command \"${zpriv_cmd}\" is invalid. Reverting to \"sudo\"."
-  zpriv_cmd='sudo'
+  zpriv_cmd=sudo
 elif (( ! ${+commands[${zpriv_cmd}]} )); then
   print -u2 "Privilege command \"${zpriv_cmd}\" is not installed. Reverting to \"sudo\"."
-  zpriv_cmd='sudo'
+  zpriv_cmd=sudo
 fi
 
 if ! zstyle -s ':zim:pacman' frontend 'zpacman_frontend'; then
-  zpacman_frontend='pacman'
+  zpacman_frontend=pacman
   zpacman_frontend_priv="${zpriv_cmd} pacman"
 elif (( ! ${+commands[${zpacman_frontend}]} )); then
   print -u2 "pacman frontend \"${zpacman_frontend}\" is invalid or not installed. Reverting to \"pacman\"."
-  zpacman_frontend='pacman'
+  zpacman_frontend=pacman
   zpacman_frontend_priv="${zpriv_cmd} pacman"
 elif [[ ${zpacman_frontend} == (pacaur|pikaur|yaourt|yay) ]]; then
   # those AUR helpers handle SUID themselves
-  zpacman_frontend_priv="${zpacman_frontend}"
+  zpacman_frontend_priv=${zpacman_frontend}
 else
   zpacman_frontend_priv="${zpriv_cmd} ${zpacman_frontend}"
 fi
